d0db70963fc3f119c22d13d2c41be0911a0d7091
McKinley <solidaritydesignsva@gmail.com>
2017-10-03 04:53:59 -0500

    Can Now Send A Zip File of all product images from a store

--- app/controller.js
+++ app/controller.js
@@ -3,15 +3,15 @@ const  fs = require('fs'),
        request = require('request'),
        archiver = require('archiver');
 
-
 module.exports = {
   showHome : (req,res) => {
 
-      /*const url = 'https://api.bigcommerce.com/stores/et6hidb37e/v2/products';
+
+
+      const url = 'https://api.bigcommerce.com/stores/et6hidb37e/v2/products';
       const id = 'ris8bseq50w7i9b0h4cynycftc4zbj8';
       const token = 'svop48vqxuyalh0ayhj8vro7vqivju7';
-
-      const options = {
+      let options = {
           url: url,
           headers: {
               'Accept': 'application/json',
@@ -20,12 +20,46 @@ module.exports = {
           }
       };
 
-      request.get(options, (err, response, body) => {
-          const data = JSON.parse(body);
-          let arr = [];
-      })*/
 
-      download('https://www.google.com/images/srpr/logo3w.png', 'images/google.png', () => {
+      request.get(options, (err, response, body) => {
+          const foo = JSON.parse(body);
+
+          foo.forEach((i, j) => {
+              let url = i.images.url;
+              options.url = url;
+              request.get(options, (err, response, body) => {
+                  let data = JSON.parse(body);
+                  for(let x = 0; x < data.length; x++){
+                      let i = data[x];
+                      let url = i.standard_url;
+                      download(url, `images/${i.product_id}-${x}`,() => {
+                          setTimeout(() => {
+                              if(j === foo.length - 1 && x === data.length -1 ){
+                                  let output = fs.createWriteStream(__dirname + '/example.zip');
+
+                                  let archive = archiver('zip', {
+                                      zlib: {level: 9}
+                                  });
+
+                                  output.on('close', () => {
+                                      res.sendFile(__dirname + '/example.zip');
+
+                                  });
+
+
+                                  archive.pipe(output);
+                                  archive.directory(path.join(__dirname, '../', '/images/'), false);
+                                  archive.finalize();
+                              }
+                          }, 1000)
+                      })
+                  }
+              })
+          })
+
+      })
+
+     /* download('https://www.google.com/images/srpr/logo3w.png', 'images/google.png', () => {
          let output = fs.createWriteStream(__dirname + '/example.zip');
 
          let archive = archiver('zip', {
@@ -44,7 +78,7 @@ module.exports = {
           archive.directory(path.join(__dirname, '../', '/images/'), false);
           archive.finalize();
 
-      });
+      });*/
 
 
   }
