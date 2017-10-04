95b63a076a72592c8ee5576e78993b193c31c633
McKinley <solidaritydesignsva@gmail.com>
2017-10-04 05:33:49 -0500

    Added static public folder

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require('express'),
+      path    = require('path');
       app     = express();
 
+
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(require('./app/routes'));
 
 
